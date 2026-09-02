fb9a5eff077f99822e76ad5367f0c4ed712bda0d
nayab khan <azstyle9157@gmail.com>
2026-09-02 09:31:43 +0530

    callbackhell

---
+++ callbackhell.js
@@ -0,0 +1,31 @@
+function test1(cb) {
+  setTimeout(() => {
+    console.log("test1");
+    cb();
+  }, 2000);
+}
+function test2(cb) {
+  setTimeout(() => {
+    console.log("test2");
+    cb();
+  }, 1000);
+}
+function test3(cb) {
+  setTimeout(() => {
+    console.log("test3");
+  }, 200);
+}
+function test4(cb) {
+  setTimeout(() => {
+    console.log("test4");
+    cb();
+  }, 100);
+}
+
+test1(() => {
+  test2(() => {
+    test3(() => {
+      test4();
+    });
+  });
+});
