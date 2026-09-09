a9001f278db3011413e35ebcbbedbb2e048541f3
nayab khan <azstyle9157@gmail.com>
2026-09-09 10:28:18 +0530

    promise function

---
+++ async_await.js
@@ -0,0 +1,26 @@
+function f1() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("HI !!");
+      resolve();
+    }, 4000);
+  });
+}
+function f2() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("ABES COLLEGE");
+      resolve();
+    }, 2000);
+  });
+}
+
+async function test() {
+  try {
+    await f1();
+    await f2();
+  } catch (err) {
+    console.log("ERROR", err);
+  }
+}
+test();
